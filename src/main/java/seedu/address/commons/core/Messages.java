e867f6d0b3e4e9c9a717e26eb33419e81c14d992
Lemonsr <46766154+Lemonsr@users.noreply.github.com>
2021-10-06 13:52:00 +0800

    Remove unnecessary break statements and change person texts to tutor/student texts

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TUTOR_DISPLAYED_INDEX = "The tutor index provided is invalid";
     public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index provided is invalid";
-    public static final String MESSAGE_TUTORS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_TUTORS_LISTED_OVERVIEW = "%1$d tutors listed!";
+    public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
 
 }
