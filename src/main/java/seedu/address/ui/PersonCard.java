0ceb0035d1af9723bf15cddf1e6b34d0ac6d817c
Chng Zi Hao <chngzihao@gmail.com>
2021-10-14 01:58:00 +0800

    Change style of lists displayed

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.LevelSubjectCode;
 
 /**
  * An UI component that displays information of a {@code Person}.
@@ -47,11 +48,11 @@ public class PersonCard extends UiPart<Region> {
         this.person = person;
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
-        phone.setText(person.getPhone().value);
-        gender.setText(person.getGender().genderSymbol);
+        phone.setText("Contact: " + person.getPhone().value);
+        gender.setText("Gender: " + person.getGender().genderSymbol);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+                .forEach(tag -> tags.getChildren().add(new Label(LevelSubjectCode.getLabel(tag.tagName))));
     }
 
     @Override
