2d9a9cb68dcb02d132ddecfe43bb89a2e13f37a0
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-10-18 01:15:15 +0800

    Change Gender to be case insensitive and added test cases to test for the validity of Gender and Qualification

---
+++ src/test/java/seedu/address/model/person/QualificationTest.java
@@ -0,0 +1,36 @@
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class QualificationTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Qualification(null));
+    }
+
+    @Test
+    public void constructor_invalidQualification_throwsIllegalArgumentException() {
+        String invalidQualification = "";
+        assertThrows(IllegalArgumentException.class, () -> new Qualification(invalidQualification));
+    }
+
+    @Test
+    public void isValidQualification() {
+        // null name
+        assertThrows(NullPointerException.class, () -> Qualification.isValidQualification(null));
+
+        // invalid qualification
+        assertFalse(Qualification.isValidQualification("")); // empty string
+        assertFalse(Qualification.isValidQualification(" ")); // spaces only
+        assertFalse(Qualification.isValidQualification("^")); // only non-alphanumeric characters
+        assertFalse(Qualification.isValidQualification("4")); // out of the valid range
+
+        // valid qualification
+        assertTrue(Qualification.isValidQualification("1")); // within valid range
+    }
+}
