dfef894f724d1c86fa8837b6558baecebd594a24
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-11-04 14:51:38 +0800

    Edit based on PR on GitHub

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -37,7 +37,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_GENDER + "GENDER "
             + "[" + PREFIX_REMARK + "REMARK] "
-            + PREFIX_TAG + "TAG>\n"
+            + PREFIX_TAG + "TAG...>\n"
             + "Example: " + COMMAND_WORD + " t "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
